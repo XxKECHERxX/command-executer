109bc3731ac99b19e52f29c2f47a561463575ae0
XxKECHERxX <zhludovanatolii@gmail.com>
2024-03-18 16:40:47 +0300

    final version

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
+const ffmpeg_executor_1 = require("./commands/ffmpeg/ffmpeg.executor");
+const console_logger_1 = require("./out/console-logger/console-logger");
 class App {
     run() {
-        console.log('Done');
+        new ffmpeg_executor_1.FfmpegExecutor(console_logger_1.ConsoleLogger.getInstance()).execute();
     }
 }
 exports.App = App;
